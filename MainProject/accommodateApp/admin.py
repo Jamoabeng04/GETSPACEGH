9fd8facce9fdccec7fa8228e8cbd4d89fbe44737
John Amoabeng <johnamoabeng7@gmail.com>
2024-10-03 00:55:01 +0000

    adding the new models to admin.py

--- MainProject/accommodateApp/admin.py
+++ MainProject/accommodateApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Category,Manager,Product,PriceRange
+from .models import Category,Manager,Product,PriceRange,Rooms,Amenities
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Manager)
 admin.site.register(Product)
 admin.site.register(PriceRange)
+admin.site.register(Rooms)
+admin.site.register(Amenities)
