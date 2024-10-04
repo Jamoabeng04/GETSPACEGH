57abd3ea31d2ea6899e8da0b677d0b0eaa3751c8
John Amoabeng <johnamoabeng7@gmail.com>
2024-10-04 12:32:30 +0000

    fixing errors

--- MainProject/accommodateApp/models.py
+++ MainProject/accommodateApp/models.py
@@ -70,7 +70,7 @@ class Product(models.Model):
         return self.name
 
 class Comment(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
     comment = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True,blank=True)
     
