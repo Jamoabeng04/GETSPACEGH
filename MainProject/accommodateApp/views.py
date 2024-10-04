388cad1d313cf5b392c63f8d3b5cf43675b39ceb
John Amoabeng <johnamoabeng7@gmail.com>
2024-10-04 22:09:57 +0000

    fixing errors

--- MainProject/accommodateApp/views.py
+++ MainProject/accommodateApp/views.py
@@ -79,7 +79,6 @@ def productDetails(request, pk):
 
         review = ProductReview.objects.create(products=products,user=request.user,stars =stars,content=content)
 
-        return redirect('/home')
     
     context = {'products': products, 'rooms1':rooms1, 'amenities':amenities}
     return render(request, 'details.html', context)
